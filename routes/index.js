66b2b2ea86467c10f267f9f48b06c9f6ed07a7ab
minhne8 <trantuanminh686@gmail.com>
2023-04-17 16:23:54 +0700

    ok

--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,9 @@ router.get('/', function(req, res, next) {
   res.redirect('https://shorturl.at/dtxPW')
 });
 
+/* GET /stripo. */
+router.get('/stripo', function(req, res, next) {
+  //res.render('index', { title: 'Express' });
+  res.redirect('https://shorturl.at/eFLO7')
+});
 module.exports = router;
